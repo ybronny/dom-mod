9040a43569408c6c0766c9531f322c52e5fde35c
ronny <ronnymwangi7@gmail.com>
2024-04-05 21:20:14 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+if (mainElement) {
+    mainElement.parentNode.removeChild(mainElement);
+}
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+
+const yourName = "YOUR-NAME"; 
+newHeader.textContent = `${yourName} is the champion`;
+
+document.body.appendChild(newHeader);
